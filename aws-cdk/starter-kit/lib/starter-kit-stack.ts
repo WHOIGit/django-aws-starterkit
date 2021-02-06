10626f708cc84884ba57c79b4bea3f96e1c27676
ethanandrews <eandrews@whoi.edu>
2021-02-06 09:51:21 -0500

    add cdk imports

--- aws-cdk/starter-kit/lib/starter-kit-stack.ts
+++ aws-cdk/starter-kit/lib/starter-kit-stack.ts
@@ -1,4 +1,7 @@
 import * as cdk from '@aws-cdk/core';
+import * as ec2 from "@aws-cdk/aws-ec2";
+import * as ecs from "@aws-cdk/aws-ecs";
+import * as ecs_patterns from "@aws-cdk/aws-ecs-patterns";
 
 export class StarterKitStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
