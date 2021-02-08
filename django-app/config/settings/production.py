fe5c58688707bb76418e3700cf0410887e5aff4a
ethanandrews <eandrews@whoi.edu>
2021-02-08 13:16:42 -0500

    update Django config for AWS

--- django-app/config/settings/production.py
+++ django-app/config/settings/production.py
@@ -1,6 +1,8 @@
 from .base import *  # noqa
 from .base import env
 
+import sys
+
 # GENERAL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
